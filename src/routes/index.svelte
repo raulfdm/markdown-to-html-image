345d2c8130b970a0a31e82050ef5e65b64d0b09a
Raul Melo <melo.raulf@gmail.com>
2021-11-18 19:57:34 +0100

    chore: add tailwind and base app

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="container max-w-screen-md mx-auto">
+  <h1>Markdown to HTML Image Parser</h1>
+
+  <div class="flex flex-row justify-between">
+    <div>
+      <input type="text" />
+    </div>
+
+    <div>
+      <input type="text" />
+    </div>
+  </div>
+
+  <hr />
+  <div>
+    <h2>Meta</h2>
+    <fieldset>
+      <label for="">Url</label>
+      <input type="text" />
+    </fieldset>
+    <fieldset>
+      <label for="">Alt</label>
+      <input type="text" />
+    </fieldset>
+    <fieldset>
+      <label for="">Width</label>
+      <input type="number" />
+    </fieldset>
+    <fieldset>
+      <label for="">Height</label>
+      <input type="number" />
+    </fieldset>
+  </div>
+</main>
+
+<style>
+  h1 {
+    font-size: 2.5rem;
+    font-weight: 700;
+    margin-bottom: 1rem;
+  }
+</style>
