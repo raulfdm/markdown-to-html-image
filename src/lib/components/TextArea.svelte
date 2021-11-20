3b445bf080913ab355eeffc2d21aab526b0ff52a
Raul Melo <melo.raulf@gmail.com>
2021-11-20 06:13:24 +0100

    feat: create basic structure

---
+++ src/lib/components/TextArea.svelte
@@ -0,0 +1,8 @@
+<textarea cols="35" rows="2" {...$$props}></textarea>
+
+<style lang="postcss">
+  textarea {
+    background-color: hsl(210deg 31% 27%);
+    @apply p-2 text-lg resize-none w-full;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,42 +1,88 @@
-<main class="container max-w-screen-md mx-auto">
+<script lang="ts">
+  import TextArea from '$lib/components/TextArea.svelte';
+
+  let markdownText = '![random picture](https://picsum.photos/seed/picsum/400/300)';
+
+  let url = getUrl();
+  let alt = getAlt();
+
+  let width = undefined;
+  let height = undefined;
+
+  let htmlString = getHtmlString();
+
+  function getUrl() {
+    const REGEX_ULR = /\((.*)\)/;
+    const [_, url] = markdownText.match(REGEX_ULR);
+
+    return url;
+  }
+
+  function getAlt() {
+    const REGEX_ALT = /\[(.*)\]/;
+    const [_, alt] = markdownText.match(REGEX_ALT);
+
+    return alt;
+  }
+
+  function getHtmlString() {
+    return `<img src="${url}" alt="${alt}" ${width ? `width="${width}"` : ''} ${
+      height ? `height="${height}"` : ''
+    }/>`;
+  }
+</script>
+
+<main class="container max-w-screen-md mx-auto h-screen">
   <h1>Markdown to HTML Image Parser</h1>
 
-  <div class="flex flex-row justify-between">
+  <picture class="grid place-items-center"> {@html htmlString} </picture>
+
+  <div class="flex flex-col justify-between">
     <div>
-      <input type="text" />
+      <h3 class="mb-2">Markdown</h3>
+      <TextArea value="{markdownText}"></TextArea>
     </div>
 
-    <div>
-      <input type="text" />
+    <div class="mt-4">
+      <h3 class="mb-2">HTML</h3>
+      <TextArea value="{htmlString}" disabled></TextArea>
     </div>
   </div>
 
-  <hr />
+  <hr class="my-6" />
+
   <div>
     <h2>Meta</h2>
-    <fieldset>
-      <label for="">Url</label>
-      <input type="text" />
-    </fieldset>
-    <fieldset>
-      <label for="">Alt</label>
-      <input type="text" />
-    </fieldset>
-    <fieldset>
-      <label for="">Width</label>
-      <input type="number" />
-    </fieldset>
-    <fieldset>
-      <label for="">Height</label>
-      <input type="number" />
-    </fieldset>
+    <div class="grid grid-cols-12 gap-4">
+      <fieldset class="col-span-12 grid grid-cols-12">
+        <label for="" class="col-span-1 flex items-center">Url</label>
+        <input type="text" value="{url}" class="col-span-11" />
+      </fieldset>
+      <fieldset class="col-span-12 grid grid-cols-12">
+        <label for="" class="col-span-1 flex items-center">Alt</label>
+        <input type="text" value="{alt}" class="col-span-11" />
+      </fieldset>
+      <fieldset class="col-span-6 grid grid-cols-12">
+        <label for="" class="col-span-2 flex items-center">Width</label>
+        <input type="number" value="{width}" class="col-span-10" />
+      </fieldset>
+      <fieldset class="col-span-6 grid grid-cols-12">
+        <label for="" class="col-span-2 flex items-center">Height</label>
+        <input type="number" value="{height}" class="col-span-10" />
+      </fieldset>
+    </div>
   </div>
 </main>
 
-<style>
+<style lang="postcss">
   h1 {
     font-size: 2.5rem;
     font-weight: 700;
     margin-bottom: 1rem;
+  }
+
+  input {
+    @apply py-2 px-4;
+    background-color: hsl(210deg 31% 27%);
   }
 </style>
